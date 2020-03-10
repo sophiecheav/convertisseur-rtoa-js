77593ba632bdef751a65dd46d9b6023d778b97e5
sophiecheav <sophie.cheavseang@gmail.com>
2020-03-10 15:16:11 +0100

    Rendre lisible le code

--- convertisseur-rtoa.js
+++ convertisseur-rtoa.js
@@ -1,45 +1,44 @@
 function convertRomanToArabic(romanNumber){
-if(romanNumber == null)
-return -1;
-let resultat = 0,
-valeur = 0, // Initialise!
-prev = 0;
 
-for(let i=0 ; i<romanNumber.length ; i++) {
-let current = char_to_int(romanNumber.charAt(i));
-if (current > prev) {
-// Undo the addition that was done, turn it into subtraction
-resultat -= 2 * valeur;
-}
-if (current !== prev) { // Different symbol?
-valeur = 0; // reset the sum for the new symbol
-}
-valeur += current; // keep adding same symbols
-resultat += current;
-prev = current;
-}
-return resultat;
-}
+  //guard
+  if(romanNumber == null || romanNumber == "") {return -1;}
 
-function char_to_int(character) {
-switch(character){
-case "I": return 1;
-case "V": return 5;
-case "X": return 10;
-case "L": return 50;
-case "C": return 100;
-case "D": return 500;
-case "M": return 1000;
-default: return -1;
-}
+  //les valeurs par défaut
+  let resultat = 0;
+  let prev = 0;
+
+  // boucle for : une condition de départ, une condition d'étape, jusqu'à l'étape finale; i++ signifie mavariable'i'+1 ; sucre syntaxique = une facilité de lgge
+  for(let position = 0 ; position < romanNumber.length ; position++) {
+    let valeurLettreRomaine = lettreDevientNombre(romanNumber.charAt(position)); //charAt =  character at (position i) ;
+
+    if (valeurLettreRomaine > prev) {
+      resultat -= 2 * prev; // resultat = resultat - 2 * valeur
+    }
+    resultat += valeurLettreRomaine;
+    prev = valeurLettreRomaine;
+  }
+
+  return resultat;
 }
 
-console.log(convertRomanToArabic("CL"));
+function lettreDevientNombre(character) { // convertit une chaîne de caractères en nb entiers
+  switch(character){
+    case "I": return 1;
+    case "V": return 5;
+    case "X": return 10;
+    case "L": return 50;
+    case "C": return 100;
+    case "D": return 500;
+    case "M": return 1000;
+    default: return -1;
+  }
+}
+
+console.log(convertRomanToArabic("V"));
 
 document.querySelector('.rtoa-btn').addEventListener('click', function() {
-  document.querySelector('.champ-chiffresromains2').value;
-  let resultat = convertRomanToArabic();
+  let valeurChiffresRomains = document.querySelector('.champ-chiffresromains2').value;
+  let resultat = convertRomanToArabic(valeurChiffresRomains);
   document.querySelector('.champ-chiffresarabes2').value = resultat;
-});
 
-convertRomanToArabic();
+});
